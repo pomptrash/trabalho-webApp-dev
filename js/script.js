8265e7aa065786780e17a06adfad0c6bec711895
João <joao.codelearning@gmail.com>
2025-04-05 21:47:28 -0300

    adição de conteúdo e ajustes
    grade de horários e mini texto sobre o projeto na home, informações sobre os professores

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ function partialsLoad(id, file) {
     fetch(file)
         .then(response => response.text())
         .then(data => {
-            document.getElementById(id).innerHTML = data
+            const item = document.getElementById(id)
+            if (item){
+                item.innerHTML = data
+            }
             darkMode()
         })
         .catch(error => console.error(`Erro ao carregar ${file}:`, error));
@@ -23,6 +26,7 @@ function darkMode() {
     const imgLogo = document.querySelector('.logo');
     const iconeDarkMode = document.querySelector('.iconeDarkMode');
     const navBar = document.querySelector('.navbar');
+    const table = document.querySelector('.table')
 
     // recupera o estado do modo noturno do localStorage e aplica ao carregar a página
     const activeDarkMode = localStorage.getItem('darkMode') === 'true';
@@ -31,20 +35,20 @@ function darkMode() {
         imgLogo.setAttribute('src', '../assets/img/logo-uninassau-light.png');
         iconeDarkMode.classList.remove('bi-moon');
         iconeDarkMode.classList.add('bi-brightness-high');
-        document.documentElement.style.setProperty('--pCardBackground', '#0a0a0a71')
-        if(navBar){
-            navBar.classList.add('bg-dark', 'navbar-dark');
-        }
+        document.documentElement.style.setProperty('--pCardBackground', '#0a0a0ad9')
+        document.documentElement.style.setProperty('--pInverseColor', 'white')
+        table?.classList.add('table-dark')
+        navBar?.classList.add('bg-dark', 'navbar-dark');
         darkModeBtn.checked = true;  // Marca o botão como ativado
     } else {
         body.classList.remove('bg-dark', 'text-light');
         imgLogo.setAttribute('src', '../assets/img/logo-uninassau-dark.png');
         iconeDarkMode.classList.add('bi-moon');
         iconeDarkMode.classList.remove('bi-brightness-high');
-        document.documentElement.style.setProperty('--pCardBackground', '#ffffff99')
-        if (navBar){
-            navBar.classList.remove('bg-dark', 'navbar-dark');
-        }
+        document.documentElement.style.setProperty('--pCardBackground', '#fafafad9')
+        document.documentElement.style.setProperty('--pInverseColor', 'black')
+        table?.classList.add('table-dark')
+        navBar?.classList.remove('bg-dark', 'navbar-dark');
     }
 
     // modo noturno (ativação/desativação)
@@ -56,19 +60,19 @@ function darkMode() {
             imgLogo.setAttribute('src', '../assets/img/logo-uninassau-light.png');
             iconeDarkMode.classList.remove('bi-moon');
             iconeDarkMode.classList.add('bi-brightness-high');
-            document.documentElement.style.setProperty('--pCardBackground', '#0a0a0a71')
-            if (navBar){
-                navBar.classList.add('bg-dark', 'navbar-dark');
-            }
+            document.documentElement.style.setProperty('--pCardBackground', '#343a40')
+            document.documentElement.style.setProperty('--pInverseColor', '#f8f9fa')
+            table?.classList.add('table-dark')
+            navBar?.classList.add('bg-dark', 'navbar-dark');
         } else {
             body.classList.remove('bg-dark', 'text-light');
             imgLogo.setAttribute('src', '../assets/img/logo-uninassau-dark.png');
             iconeDarkMode.classList.add('bi-moon');
             iconeDarkMode.classList.remove('bi-brightness-high');
-            document.documentElement.style.setProperty('--pCardBackground', '#ffffff71')
-            if (navBar){
-                navBar.classList.remove('bg-dark', 'navbar-dark');
-            }
+            document.documentElement.style.setProperty('--pCardBackground', '#f8f9fa')
+            document.documentElement.style.setProperty('--pInverseColor', '#343a40')
+            table?.classList.remove('table-dark')
+            navBar?.classList.remove('bg-dark', 'navbar-dark');
         }
 
         // salva o estado atual do modo no localStorage
